013884f88c01dba5c33559079729add06b6c19ed
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-23 18:47:00 -0700

    Moved main_test to bot_test.

---
+++ bot/bot_test.py
@@ -0,0 +1,16 @@
+import bot
+
+import getpass
+
+bot.ImportTest()
+
+test_bot = bot.Bot(
+    "192.168.1.14",
+    "bolt",
+    getpass.getpass("pwd: ")
+)
+
+ifconfig = test_bot.send_command("ifconfig")
+print(type(ifconfig))
+
+print(ifconfig)
