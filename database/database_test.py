30c8d116957e268b5f4922dd54d62e132579a198
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-24 09:02:25 -0700

    Added read databaseio tests.

--- database/database_test.py
+++ database/database_test.py
@@ -1,8 +1,18 @@
 from database import database
 
+# TestWriteToMemory - test functionality of writetomemory method
 def TestWriteToMemory():
-    db = database.Database()
+    db = database.Database() # Init new database
 
-    db.WriteToMemory()
+    db.WriteToMemory() # Write Database to memory
 
-TestWriteToMemory()
+# TestReadFromMemory - test functionality of readfrommemory method
+def TestReadFromMemory():
+    db = database.Database() # Init new database
+
+    db.WriteToMemory() # Write Database to memory
+
+    db.ReadFromMemory() # Attempt to read database from memory
+
+TestWriteToMemory() # Run write test
+TestReadFromMemory() # Run read test
