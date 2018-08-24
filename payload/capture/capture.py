009b8640c10df5107c5491aec9c83838a0be6b1d
Matt N <mattnappo@gmail.com>
2018-08-24 01:15:16 -0400

    Starting screencap and webcam cap payloads

--- payload/capture/capture.py
+++ payload/capture/capture.py
@@ -1,6 +1,16 @@
 import os, sys
 
-class ScreenCapture:
+class Screen:
+    def capture(self, file_name):
+        if sys.platform == "darwin":
+            os.system("screencapture " + file_name + ".png")
+        elif sys.platform.system() == "Windows":
+            from PIL import ImageGrab
+            snapshot = ImageGrab.grab()
+            save_path = os.getcwd() + "\\" + file_name + ".jpg"
+            snapshot.save(save_path)
+
+class Webcam:
     def capture(self, file_name):
         if sys.platform == "darwin":
             os.system("screencapture " + file_name + ".png")
@@ -10,5 +20,12 @@ class ScreenCapture:
             save_path = "C:\\Users\\YourUser\\Desktop\\MySnapshot.jpg"
             snapshot.save(save_path)
 
-sc = ScreenCapture()
-sc.capture("asd")
+def screen():
+    scr = Screen()
+    scr.capture("test")
+def cam():
+    webc = Webcam()
+    webc.capture("test")
+
+# screen()
+cam()
