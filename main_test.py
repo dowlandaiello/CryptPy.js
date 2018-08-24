688841c9dd63c7e5d3b75ed746d07a29b444fcde
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-23 17:33:51 -0700

    Removed a few more comments.

--- main_test.py
+++ main_test.py
@@ -1,17 +1,2 @@
-# thx
-# np
-
-# going to eat dinner now 
-
-# I'll be back in like 45 minutes
-
-# feel free to work on it while I'm gone
-
-# I'll close liveshare now
-#
-  w# have fun
-
-  # ok
-
 Bot("192.168.")
 
