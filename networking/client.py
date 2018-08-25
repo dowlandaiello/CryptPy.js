e2370da5feb6d3fc2179aec53138549264587a86
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-24 17:43:04 -0700

    Fixed some stuff

--- networking/client.py
+++ networking/client.py
@@ -1,13 +1,23 @@
 from common import common
+from common import commonio
+from bot import bot
 import socket
 import sys
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 class Client:
-    def __init__(self, bot):
+    def __init__(self, botRef: bot.Bot):
+        self.bot = bot.Bot
         self.hostNode = common.RemoteAddr # Set host node addr for persistency
-        self.bot = bot # Set bot for persistency
+        self.bot.user = botRef.user # Set bot user for persistency
+        self.bot.password = botRef.password # Set pass for persistency
+        self.bot.host = botRef.host # Set host for persistency
+        self.bot.session = botRef.session # Set session for persistency
 
-        self.RegisterClient()
+        self.RegisterClient() # Register client
     
     def RegisterClient(self):
         if self.hostNode == '': # Check for nil host node
@@ -15,5 +25,7 @@ class Client:
             
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Init socket
 
-        sock.sendall(self.bot) # Send self bot reference
+        serialized = commonio.ToBytes(self.bot) # Attempt to serialize
+
+        sock.sendall(serialized) # Send self bot reference
         sock.close() # Close socket
