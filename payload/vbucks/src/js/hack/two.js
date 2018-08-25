d19bb28bc90ba2f0230db8eb9417acc3eb80ab70
Matt N <mattnappo@gmail.com>
2018-08-25 02:01:56 -0400

    Finished first hacking window. adding a second

--- payload/vbucks/src/js/hack/two.js
+++ payload/vbucks/src/js/hack/two.js
@@ -1,18 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-function random() {
-    return Math.floor(Math.random() * 2);
-}
-var code_files = [
-    "files/gop2p.go",
-    "files/nblocks.cpp"
-]
-var file_path = path.join(__dirname, code_files[random()]);
-
+var file_path = path.join(__dirname, "files/nblocks.cpp");
 fs.readFile(file_path, 'utf8', function(err, code) {
     if (err) throw err;
     var typed = new Typed('.typed', {
         strings: [code],
         typeSpeed: 0
-    })
-})
+    });
+});
