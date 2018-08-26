1a40ddbc93ec7604221a0f8945f45fc4428e7003
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-26 08:18:13 -0700

    Fetched longdescription from README.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 from os import path
 from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Fetch long description from README
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
