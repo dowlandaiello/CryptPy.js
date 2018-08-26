89816beeb478baf48bf57379bf8096511994afd7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-25 21:44:26 -0700

    Added UpNP support.

--- common/common.py
+++ common/common.py
@@ -1,4 +1,5 @@
 import sys, os
+import upnpclient
 
 RemoteAddr = "localhost" # Add server address
 
@@ -9,3 +10,8 @@ def disablePrint():
 # Restore calls to print()
 def enablePrint():
     sys.stdout = sys.__stdout__
+
+# Attempts to forward given port
+def forwardPort(port):
+    devices = upnpclient.discover() # Get devices
+    devices[0].WANIPConn1.AddPortMapping(port)
