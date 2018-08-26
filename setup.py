e8fac2ff5ed3b8f6dc9149d216ee3ef6bd55f051
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-26 08:33:36 -0700

    Setup backup.

--- setup.py
+++ setup.py
@@ -6,5 +6,56 @@ here = path.abspath(path.dirname(__file__))
 
 # Fetch long description from README
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    long_description = f.read() # Read from README
 
+# Basic project info
+setup (
+    # Name of project on pypi.org
+    #
+    # e.g. pip install CryptPy
+    name='CryptPy',
+
+    # Version
+    #
+    # e.g. pip install CryptPy >= 0.1.0
+    version='0.1.0',
+
+    # Short description fetched from GitHub page
+    description='Dynamically programmable Python/JS botnet (educational purposes only).',
+
+
+    # Longer description fetched from README
+    #
+    # Used in 'description' field on https://packaging.python.org/specifications/
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    # Links to project homepage (github)
+    #
+    # TODO: Probably going to be replaced with a github.io page later
+    url='https://github.com/mitsukomegumi/CryptPy.js',
+
+    # Does this even need a comment? Probably not.
+    author='Mitsuko Megumi, Matt Nappo',
+
+    # 'My' email
+    author_email='mitsukomegumii@gmail.com',
+
+    classifiers=[
+        # Obviously
+        'Development Status :: 3 - Alpha',
+
+        # Because this tool is just for 'educational' use
+        'Intended Audience :: Developers',
+
+        # Ree, GPL
+        'License :: GPL-3.0 License',
+        
+        # Python 3
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
