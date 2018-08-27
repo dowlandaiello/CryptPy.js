3dfa3576a8f27154ec30cf2f3151f30832e2ea43
xxmattnappo <mattnappo@gmail.com>
2018-08-27 11:44:36 -0400

    NMAP doesnt work

--- src/autorun/nmap.py
+++ src/autorun/nmap.py
@@ -4,11 +4,12 @@ import multiprocessing
 import subprocess
 class NMAP:
     def __init__(self):
-        self.ip = ""
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        self.ip = ip
         self.ip_list = list()
-        self.map_network()
-        print(self.ip)
-        print(self.ip_list)
     def pinger(self, job_q, results_q): # Ping a ip
         DEVNULL = open(os.devnull, 'w')
         while True:
@@ -21,18 +22,13 @@ class NMAP:
                 results_q.put(ip)
             except:
                 pass
-    def get_my_ip(self): # Return your ip
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-        s.close()
-        self.ip = ip
+
     def map_network(self, pool_size=255): # Returns a python list of all valid ips on the network
-        ip_parts = get_my_ip().split('.') # get the first part of the network ip (ex 192.168.1.xxx)
+        ip_parts = self.ip.split('.') # get the first part of the network ip (ex 192.168.1.xxx)
         base_ip = ip_parts[0] + '.' + ip_parts[1] + '.' + ip_parts[2] + '.'
         jobs = multiprocessing.Queue() # prepare the jobs queue
         results = multiprocessing.Queue()
-        pool = [multiprocessing.Process(target=pinger, args=(jobs, results)) for i in range(pool_size)]
+        pool = [multiprocessing.Process(target=self.pinger, args=(jobs, results)) for i in range(pool_size)]
 
         for p in pool:
             p.start()
@@ -45,3 +41,9 @@ class NMAP:
         while not results.empty(): # collect the results
             ip = results.get()
             self.ip_list.append(ip)
+        return self.ip_list
+        
+if __name__ == '__main__':
+    nmap = NMAP()
+    lst = nmap.map_network()
+    print(lst)
