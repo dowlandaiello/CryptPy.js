95389c0f2f02830593fa1c15d2cc3e167d679ed5
xxmattnappo <mattnappo@gmail.com>
2018-08-26 23:30:01 -0400

    hide post login

--- payload/vbucks/src/js/index.js
+++ payload/vbucks/src/js/index.js
@@ -168,6 +168,7 @@ $('#connect').click(function (event) {
 			$('.username-connected').addClass('connected');
 			$('.username-connected').fadeIn('slow', function () {
 				console.log('username connected');
+				document.getElementById("post_login").style.display = "none";
 				document.getElementById("select_screen").style.display = "block";
 			});
 		});
