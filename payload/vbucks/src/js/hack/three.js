4fd77a9becbf54231b8e7d1635a39ef59828b983
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-27 17:30:54 -0700

    Fixed your enter key [again].

--- payload/vbucks/src/js/hack/three.js
+++ payload/vbucks/src/js/hack/three.js
@@ -1,19 +1,20 @@
 /*jshint esversion: 6 */
 
-const fs = require('fs');
-const path = require('path');
-const exec = require('child_process').exec;
+const remote = require('electron').remote;
+const main = remote.require('./main.js');
 
-function execute(command, callback) {
-    exec(command, (error, stdout, stderr) => { 
-        callback(stdout); 
+var os = main.get_os();
+
+main.execute('brew install ', (output) => {
+    var typed = new Typed('.typed', {
+        strings: [output],
+        typeSpeed: 0
     });
-}
+});
 
 function close() {
     console.log("before closing");
     main.close_hacking_windows();
 }
-const remote = require('electron').remote;
-const main = remote.require('./main.js');
+
 setTimeout(close, 7000);
