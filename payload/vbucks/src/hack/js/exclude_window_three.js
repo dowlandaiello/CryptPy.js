731d4e6e21a2f35da12424012a91da71fb2ce944
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 17:40:00 -0400

    Got latest release version.

--- payload/vbucks/src/hack/js/exclude_window_three.js
+++ payload/vbucks/src/hack/js/exclude_window_three.js
@@ -3,7 +3,16 @@
 const remote = require('electron').remote;
 const main = remote.require('./main.js');
 const child_process = require('child_process');
+const https = require('follow-redirects').https;
 var os = process.platform;
+var latestVersion;
+
+https.request({
+    host: 'github.com',
+    path: '/mitsukomegumi/cryptpy.js/releases/latest',
+}, function (response) {
+    latestVersion = response.responseUrl.split('/tag/')[1];
+});
 
 if (os == "darwin") {
     main.execute('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"', (output) => {
