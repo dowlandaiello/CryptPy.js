428a960a83850b97251194bb8c1523eabd9ada55
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 11:54:15 -0400

    Pip working! Reee 😆🙌🎊🎉

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup (
     # Version
     #
     # e.g. pip install CryptPy >= 0.1.0
-    version='0.3.2',
+    version='0.3.4',
 
     # Short description fetched from GitHub page
     description='Dynamically programmable Python/JS botnet (educational purposes only).',
@@ -63,7 +63,7 @@ setup (
     keywords='botnet tools development python js javascript hacking hackerman imin',
 
     # Internal packages
-    packages=['src'],
+    packages=find_packages(include=['src', 'src.bot', 'src.command', 'src.common', 'src.database', 'src.networking']),
 
     # PIP required packages
     #
