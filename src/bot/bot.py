48797c9b8dd65ebdfd2e4511af32556402cf1e16
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 09:09:09 -0400

    Caught socket exception.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -29,14 +29,17 @@ class Bot:
 
     # sending a command to the client
     def send_command(self, command):
-        print('\nattempting on host '+self.host+'\n')
-        url = "https://"+self.host+":3000/execute" # Get addr
+        try:
+            print('\nattempting on host '+self.host+'\n')
+            url = "https://"+self.host+":3000/execute" # Get addr
 
-        data = {'command': command} # Set request data
-        headers = {'Content-Type': 'application/json'} # Init request headers
-        r = requests.post(url, data=json.dumps(data), headers=headers, verify=false) # Send request
+            data = {'command': command} # Set request data
+            headers = {'Content-Type': 'application/json'} # Init request headers
+            r = requests.post(url, data=json.dumps(data), headers=headers, verify=false) # Send request
 
-        return r.json() # Return response
+            return r.json() # Return response
+        except Exception as e:
+            print(e)
 
     # dump to bytes
     def to_bytes(self):
