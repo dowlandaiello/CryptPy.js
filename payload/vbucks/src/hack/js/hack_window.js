fda06841b083e56e3fe601b422dcc9fbda5ccd93
xxmattnappo <mattnappo@gmail.com>
2018-08-29 12:08:40 -0400

    Fixed struct issues

--- payload/vbucks/src/hack/js/hack_window.js
+++ payload/vbucks/src/hack/js/hack_window.js
@@ -1,5 +1,5 @@
 const remote = require('electron').remote;
-const main = remote.require('./main.js');
+const main = remote.require('../main.js');
 const fs = require('fs');
 const path = require('path');
 
